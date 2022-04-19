a6519d3ab5f2af170f3cbe0260d7757c34a47fcd
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2022-04-19 13:16:57 +0200

    fixed configs

--- home/.config/fish/conf.d/01_path.fish
+++ home/.config/fish/conf.d/01_path.fish
@@ -1,18 +1,18 @@
 
-export EDITOR=nvim
-export GIT_EDITOR=nvim
+set --export EDITOR nvim
+set --export GIT_EDITOR nvim
 
-export PATH=$PATH:$HOME/Programs/bin
-export PATH=$PATH:$HOME/.bin
+fish_add_path -Pa $HOME/Programs/bin
+fish_add_path -Pa $HOME/.bin
 
 # Android
-export ANDROID_HOME=$HOME/.android/Sdk
-export ANDROID_SDK_HOME=$HOME/.android
-export ANDROID_AVD_HOME=$HOME/.android/avd
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
-export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/platform-tools
+set --export ANDROID_HOME $HOME/.android/Sdk
+set --export ANDROID_SDK_HOME $HOME/.android
+set --export ANDROID_AVD_HOME $HOME/.android/avd
+fish_add_path -Pa $ANDROID_HOME/tools
+fish_add_path -Pa $ANDROID_HOME/tools/bin
+fish_add_path -Pa $ANDROID_HOME/emulator
+fish_add_path -Pa $ANDROID_HOME/platform-tools
 
-export PATH=$PATH:$HOME/.yarn/bin
-export PATH=$PATH:$HOME/.cargo/bin
+fish_add_path -Pa $HOME/.yarn/bin
+fish_add_path -Pa $HOME/.cargo/bin
