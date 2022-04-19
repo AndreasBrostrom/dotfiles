bd225303679498dfbb089aecf892c0cc9ee75922
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2022-04-19 13:00:23 +0200

    fixed iff

--- home/.config/fish/conf.d/01_path.fish
+++ home/.config/fish/conf.d/01_path.fish
@@ -6,15 +6,13 @@ export PATH=$PATH:$HOME/Programs/bin
 export PATH=$PATH:$HOME/.bin
 
 # Android
-if [ -d $HOME/.android ]; then
-    export ANDROID_HOME=$HOME/.android/Sdk
-    export ANDROID_SDK_HOME=$HOME/.android
-    export ANDROID_AVD_HOME=$HOME/.android/avd
-    export PATH=$PATH:$ANDROID_HOME/tools
-    export PATH=$PATH:$ANDROID_HOME/tools/bin
-    export PATH=$PATH:$ANDROID_HOME/emulator
-    export PATH=$PATH:$ANDROID_HOME/platform-tools
-fi
+export ANDROID_HOME=$HOME/.android/Sdk
+export ANDROID_SDK_HOME=$HOME/.android
+export ANDROID_AVD_HOME=$HOME/.android/avd
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 export PATH=$PATH:$HOME/.yarn/bin
 export PATH=$PATH:$HOME/.cargo/bin
