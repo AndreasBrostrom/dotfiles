6faa0d66414a4fb577cd94826eda75b295c6257e
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2022-05-02 14:56:58 +0200

    ssh config and interacive shell

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,9 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+
+if not status is-interactive
+    # Commands to run when not in interactive sessions can go here
 end
 
-starship init fish | source
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+end
