ea326e08e3d76272916f4c6970cc58159bd74b59
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2022-05-06 10:27:41 +0200

    cleaned configs

--- home/.config/fish/conf.d/91_ssh-agent.fish
+++
@@ -1,9 +0,0 @@
-
-if status is-interactive
-  if test -z "$SSH_AGENT_PID"
-    eval "$(ssh-agent -c)" >/dev/null
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-    set -Ux SSH_AGENT_PID $SSH_AGENT_PID
-    set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
-  end
-end
