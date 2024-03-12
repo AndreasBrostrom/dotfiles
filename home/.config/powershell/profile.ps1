20bb203fceea82dd6b4e477a873c49657857b35a
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-03-12 11:42:54 +0100

    Standardized my code

--- home/.config/powershell/profile.ps1
+++ home/.config/powershell/profile.ps1
@@ -1,16 +1,16 @@
 # Globlas
-$ENV:USERNAME = $ENV:USER
-$ENV:USERPROFILE = $(Get-Variable HOME -valueOnly)              # assume homefolder folder
-$ENV:LOCALAPPDATA = "$(Get-Variable HOME -valueOnly)/.config"   # assume config folder
+$Env:USERNAME = $Env:USER
+$Env:USERPROFILE = $(Get-Variable HOME -valueOnly)              # assume homefolder folder
+$Env:LOCALAPPDATA = "$(Get-Variable HOME -valueOnly)/.config"   # assume config folder
 
 
 # RC if one exist
-if (Test-Path "$ENV:USERPROFILE/.pwshrc.ps1" -PathType leaf) {
-    . "$ENV:USERPROFILE/.pwshrc.ps1"
+if (Test-Path "$Env:USERPROFILE/.pwshrc.ps1" -PathType leaf) {
+    . "$Env:USERPROFILE/.pwshrc.ps1"
 }
-if (Test-Path "$ENV:LOCALAPPDATA/powershell/rc.ps1" -PathType leaf) {
-    . "$ENV:LOCALAPPDATA/powershell/rc.ps1"
+if (Test-Path "$Env:LOCALAPPDATA/powershell/rc.ps1" -PathType leaf) {
+    . "$Env:LOCALAPPDATA/powershell/rc.ps1"
 }
-if (Test-Path "$ENV:LOCALAPPDATA/powershell/aliases.ps1" -PathType leaf) {
-    . "$ENV:LOCALAPPDATA/powershell/aliases.ps1"
+if (Test-Path "$Env:LOCALAPPDATA/powershell/aliases.ps1" -PathType leaf) {
+    . "$Env:LOCALAPPDATA/powershell/aliases.ps1"
 }
