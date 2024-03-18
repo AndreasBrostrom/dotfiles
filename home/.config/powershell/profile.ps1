dcef97052129a1c95130b1428002fb1f38b7eb16
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2024-03-18 11:09:16 +0100

    updated for new usage

--- home/.config/powershell/profile.ps1
+++ home/.config/powershell/profile.ps1
@@ -3,14 +3,6 @@ $Env:USERNAME = $Env:USER
 $Env:USERPROFILE = $(Get-Variable HOME -valueOnly)              # assume homefolder folder
 $Env:LOCALAPPDATA = "$(Get-Variable HOME -valueOnly)/.config"   # assume config folder
 
-
-# RC if one exist
-if (Test-Path "$Env:USERPROFILE/.pwshrc.ps1" -PathType leaf) {
-    . "$Env:USERPROFILE/.pwshrc.ps1"
-}
-if (Test-Path "$Env:LOCALAPPDATA/powershell/rc.ps1" -PathType leaf) {
-    . "$Env:LOCALAPPDATA/powershell/rc.ps1"
-}
-if (Test-Path "$Env:LOCALAPPDATA/powershell/aliases.ps1" -PathType leaf) {
-    . "$Env:LOCALAPPDATA/powershell/aliases.ps1"
+if (Test-Path "$Env:LOCALAPPDATA/powershell/config.ps1" -PathType leaf) {
+    . "$Env:USERPROFILE/.config/powershell/config.ps1"
 }
