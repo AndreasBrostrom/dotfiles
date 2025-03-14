cb91589fb33e48eddd513e1817b97a31e1ce814e
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2025-03-14 20:41:38 +0100

    added push and cleaned

--- home/.config/fish/conf.d/06_aliases_git.fish
+++ home/.config/fish/conf.d/06_aliases_git.fish
@@ -1,32 +1,42 @@
 alias gs='git status'
+
 alias gc='git checkout'
 alias gcm="git checkout (git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')"
+
 alias gcpr='git checkout-pr'
+
 alias gb='git branch'
+
 alias gf='git fetch --all --prune'
+
 alias gr='git rebase'
 alias grm='git rebase origin/main'
+
 alias gpull='git pull'
-alias gpu='git push'
+
+alias gpush='git push'
+alias gpu='gpush'
 alias gpuf='git push fork'
 alias gpuff='eval git push --set-upstream fork (git rev-parse --abbrev-ref HEAD)'
+
 alias gp='git stash'
 alias gpp='git stash pop'
 
-alias gfr='gf; gr'                                 # fetch rebase
-alias gfm='gf; grm'                                # fetch rebase:orgin/master
-alias gfrp='gp; gf; grm'                           # stash fetch rebase:orgin/master
-alias gfrpp='gp; gf; grm; gpp'                     # stash fetch rebase:orgin/master pop
-alias gcp='gp; gc'                                 # stash checkout
-alias gpullp='gp; gpull'                           # stash pull
-alias gpullpp='gp; gpull; gpp'                     # stash pull pop
+# functions
+alias gfr='gf; gr'                        # fetch rebase
+alias gfm='gf; grm'                       # fetch rebase:orgin/master
+alias gfrp='gp; gf; grm'                  # stash fetch rebase:orgin/master
+alias gfrpp='gp; gf; grm; gpp'            # stash fetch rebase:orgin/master pop
+alias gcp='gp; gc'                        # stash checkout
+alias gpullp='gp; gpull'                  # stash pull
+alias gpullpp='gp; gpull; gpp'            # stash pull pop
 
-function gcpp                                      # stash checkout pop
+function gcpp                             # stash checkout pop
     eval gp; eval gc $argv; eval gpp ;
 end
-alias gcmp='gp; gcm'                               # stash checkout:master
-alias gcmpp='gp; gcm; gpp'                         # stash checkout:master pop
-function gcprfr                                    # fetch checkout:PR rebase
+alias gcmp='gp; gcm'                      # stash checkout:master
+alias gcmpp='gp; gcm; gpp'                # stash checkout:master pop
+function gcprfr                           # fetch checkout:PR rebase
     eval gf; eval gcpr $argv; eval gr ;
 end
 
