0227f2f4504b4203d864a41df65df457c6161008
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2025-05-12 11:56:29 +0200

    added help and cleanup for sitrep

--- home/.config/fish/functions/upgrade.fish
+++ home/.config/fish/functions/upgrade.fish
@@ -1,25 +1,41 @@
 function upgrade
+    if test (count $argv) -gt 0
+        # Loop through all arguments
+        for arg in $argv
+            switch $arg
+                case "--help"
+                    echo -e "\033[1mUsage: upgrade\033[0m [--help]"
+                    echo -e "This script attempts to fully upgrade your system using the OS-specific package manager."
+                    echo -e "If snap, flatpak or sitrep exist this will also upgrade."
+                    echo -e "\n\033[1mOptions:\033[0m"
+                    echo -e "  \033[1;32m--help\033[0m         Show this help message and exit"
+                    return
+            end
+        end
+    end
+
     function _fn_upgrade_deconstructor
         functions -e _fn_upgrade_flatpak
         functions -e _fn_upgrade_snap
+        functions -e _fn_upgrade_sitrep
         functions -e _fn_upgrade_deconstructor
     end
 
     function _fn_upgrade_flatpak
         if exist flatpak
-            echo -e "\033[32mflatpak\033[0m"
+            echo -e "\033[1;32mflatpak\033[0m"
             sudo flatpak update -y
         end
     end
     function _fn_upgrade_snap
         if exist snap
-            echo -e "\033[32msnap\033[0m"
+            echo -e "\033[1;32msnap\033[0m"
             sudo snap refresh
         end
     end
     function _fn_upgrade_sitrep
         if exist sitrep
-            echo -e "\033[32msitrep\033[0m"
+            echo -e '\033[1;32msitrep\033[0m'
             sitrep -u
         end
     end
@@ -27,7 +43,6 @@ function upgrade
     echo -e '\033[1mFully upgrading system and packages...\033[0m'
 
     # Arch
-    # pacman
     if exist pacman
         # pacman paru
         if exist paru
@@ -36,7 +51,7 @@ function upgrade
             paru -Syyu --sudoloop --noconfirm --color=always
             _fn_upgrade_flatpak
             _fn_upgrade_snap
-            exist notify-send && notify-send 'paru' '<i>System upgrade is complet.</i>' --urgency=normal >/dev/null 2>/dev/null
+            exist notify-send && notify-send 'paru' '<i>System upgrade is completed.</i>' --urgency=normal >/dev/null 2>/dev/null
             echo -e '\033[1mFull upgrade completed\033[0m'
             set orphan (paru -Qtdq)
             if ! test (count $orphan) -eq 0
@@ -57,7 +72,7 @@ function upgrade
             yay -Syyu --sudoloop --noconfirm --color=always
             exist flatpak && _fn_upgrade_flatpak
             exist snap && _fn_upgrade_snap
-            exist notify-send && notify-send 'yay' '<i>System upgrade is complet.</i>' --urgency=normal >/dev/null 2>/dev/null
+            exist notify-send && notify-send 'yay' '<i>System upgrade is completed.</i>' --urgency=normal >/dev/null 2>/dev/null
             echo -e '\033[1mFull upgrade completed\033[0m'
             set orphan (yay -Qtdq)
             if ! test (count $orphan) -eq 0
@@ -76,7 +91,7 @@ function upgrade
         sudo pacman -Syyu --noconfirm --color=always
         _fn_upgrade_flatpak
         _fn_upgrade_snap
-        exist notify-send && notify-send 'pacman' '<i>System upgrade is complet.</i>' --urgency=normal >/dev/null 2>/dev/null
+        exist notify-send && notify-send 'pacman' '<i>System upgrade is completed.</i>' --urgency=normal >/dev/null 2>/dev/null
         echo -e '\033[1mFull upgrade completed\033[0m'
         set orphan (pacman -Qtdq)
         if ! test (count $orphan) -eq 0
@@ -90,7 +105,7 @@ function upgrade
         return
     end
 
-    # Ubuntu
+    # Debian
     if exist apt
         # termux pkg (apt wraper)
         if exist pkg
@@ -114,6 +129,22 @@ function upgrade
         _fn_upgrade_snap
         _fn_upgrade_sitrep
         exist notify-send && notify-send 'apt' '<i>System upgrade is complet.</i>' --urgency=normal >/dev/null 2>/dev/null
+        echo -e "\033[1mAll updates are completed.\033[0m"
+        _fn_upgrade_deconstructor
+        return
+    end
+
+    # Fedora
+    if exist dfn
+        # dfn (default)
+        sudo -v
+        echo -e "\033[32mdfn\033[0m"
+        sudo dnf upgrade -y
+
+        _fn_upgrade_flatpak
+        _fn_upgrade_snap
+        _fn_upgrade_sitrep
+        exist notify-send && notify-send 'dfn' '<i>System upgrade is completed.</i>' --urgency=normal >/dev/null 2>/dev/null
         echo -e "\033[1mAll updates are completed.\033[0m"
         _fn_upgrade_deconstructor
         return
