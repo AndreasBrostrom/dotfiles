3200168fc9b8d6636c860696dff9721febd8c2af
AndreasBrostrom <andreas.brostrom.ce@gmail.com>
2025-07-01 23:14:14 +0200

    fixed lock alias

--- home/.config/fish/conf.d/09_aliases_misc.fish
+++ home/.config/fish/conf.d/09_aliases_misc.fish
@@ -9,7 +9,11 @@ alias adb-r='adb-reverse'
 alias adb-url='adb shell am start -a android.intent.action.VIEW -d '
 
 
-# Arch
-alias lock='blurlock'
-alias disable-lock='pkill -9 -f xautolock'
-alias disable-autolock='disable-lock'
+# lock screen aliases
+if test "$XDG_CURRENT_DESKTOP" = "XFCE" -o "$XDG_SESSION_DESKTOP" = "xfce"
+    alias lock 'xflock4'
+    alias disable-autolock 'pkill -9 -f xflock4'
+else if test "$XDG_CURRENT_DESKTOP" = "i3" -o "$XDG_SESSION_DESKTOP" = "i3"
+    alias lock 'i3exit lock'
+    alias disable-autolock 'pkill -9 -f xautolock'
+end
